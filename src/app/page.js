c517bded21e1a5acdad789bcacfb5ac34005cf61
Ilene <ileneh889@yahoo.com>
2025-03-14 14:11:47 +0800

    style: font setup

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default async function Home() {
   return (
     <>
       <SidebarInset>
+        <h1 className="font-bold">孿生城市</h1>
         <div className="flex flex-wrap w-full px-6 gap-4">
           {
             Array.from({ length: 10 }).map((_, index) => (
